af6d6ed363c13b7cafc209d0b0e9dff8ba502e98
Markus Phantha <markus.a.phantha@gmail.com>
2023-10-20 10:34:42 +0300

    kinda ish clickable btns

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 
 export default function App() {
+  const [isClicked, setClickState] = useState(false);
+
+  const handleClick = () => {
+    setClickState(!isClicked);
+  };
+
+  const card = isClicked ? "static-card" : "card";
+
   return (
     <div className="place-content-center p-40 m-60 shadow-xl">
       <h1 className="text-5xl font-extrabold">October 2023</h1>
@@ -25,44 +33,46 @@ export default function App() {
           <button className="card">1</button>
         </div>
         <div className="flex justify-between pt-2 m-10">
-          <button className="card">2</button>
-          <button className="card">3</button>
-          <button className="card">4</button>
-          <button className="card">5</button>
-          <button className="card">6</button>
-          <button className="card">7</button>
-          <button className="card">8</button>
+          <button className={card} onClick={handleClick}>
+            2
+          </button>
+          <button className={card} onClick={handleClick}>3</button>
+          <button className={card} onClick={handleClick}>4</button>
+          <button className={card} onClick={handleClick}>5</button>
+          <button className={card} onClick={handleClick}>6</button>
+          <button className={card} onClick={handleClick}>7</button>
+          <button className={card} onClick={handleClick}>8</button>
         </div>
         <div className="flex justify-between pt-2 m-10">
-          <button className="card">9</button>
-          <button className="card">10</button>
-          <button className="card">11</button>
-          <button className="card">12</button>
-          <button className="card">13</button>
-          <button className="card">14</button>
-          <button className="card">15</button>
+          <button className={card} onClick={handleClick}>9</button>
+          <button className={card} onClick={handleClick}>10</button>
+          <button className={card} onClick={handleClick}>11</button>
+          <button className={card} onClick={handleClick}>12</button>
+          <button className={card} onClick={handleClick}>13</button>
+          <button className={card} onClick={handleClick}>14</button>
+          <button className={card} onClick={handleClick}>15</button>
         </div>
         <div className="flex justify-between pt-2 m-10">
-          <button className="card">16</button>
-          <button className="card">17</button>
-          <button className="card">18</button>
-          <button className="card">19</button>
-          <button className="card">20</button>
-          <button className="card">21</button>
-          <button className="card">22</button>
+          <button className={card} onClick={handleClick}>16</button>
+          <button className={card} onClick={handleClick}>17</button>
+          <button className={card} onClick={handleClick}>18</button>
+          <button className={card} onClick={handleClick}>19</button>
+          <button className={card} onClick={handleClick}>20</button>
+          <button className={card} onClick={handleClick}>21</button>
+          <button className={card} onClick={handleClick}>22</button>
         </div>
         <div className="flex justify-between pt-2 m-10">
-          <button className="card">23</button>
-          <button className="card">24</button>
-          <button className="card">25</button>
-          <button className="card">26</button>
-          <button className="card">27</button>
-          <button className="card">28</button>
-          <button className="card">29</button>
+          <button className={card} onClick={handleClick}>23</button>
+          <button className={card} onClick={handleClick}>24</button>
+          <button className={card} onClick={handleClick}>25</button>
+          <button className={card} onClick={handleClick}>26</button>
+          <button className={card} onClick={handleClick}>27</button>
+          <button className={card} onClick={handleClick}>28</button>
+          <button className={card} onClick={handleClick}>29</button>
         </div>
         <div className="flex justify-between pt-2 m-10">
-          <button className="card">30</button>
-          <button className="card">31</button>
+          <button className={card} onClick={handleClick}>30</button>
+          <button className={card} onClick={handleClick}>31</button>
           <button className="card text-sky-400/75">1</button>
           <button className="card text-sky-400/75">2</button>
           <button className="card text-sky-400/75">3</button>
