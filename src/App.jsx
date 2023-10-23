ae1364efc12e6187e808eaba9ab27174f137f06b
Markus Phantha <markus.a.phantha@gmail.com>
2023-10-23 11:43:11 +0300

    instead of a month this has become seven weeks

--- src/App.jsx
+++ src/App.jsx
@@ -4,73 +4,79 @@ import ColoredClickableBtn from "./components/ColoredClickableBtn"
 export default function App() {
 
   return (
-    <div className="place-content-center p-20 m-52 shadow-xl">
-      <h1 className="text-5xl font-extrabold">October 2023</h1>
-      <h3 className="center mt-10">MY AVAILABILITY THIS MONTH</h3>
-      <div>
-        <ul className="flex justify-between pt-20 mb-10">
-          <li className="">Monday</li>
-          <li className="">Tuesday</li>
-          <li className="">Wednesday</li>
-          <li className="">Thursday</li>
-          <li className="">Friday</li>
-          <li className="">Saturday</li>
-          <li className="">Sunday</li>
-        </ul>
+    <div className="place-content-center p-16 m-32 shadow-xl">
+      <h1 className="text-3xl font-bold text-orange-400 pb-14 text-center">
+        MY AVAILABILITY FOR 7 WEEKS
+      </h1>
+      <div className="flex justify-between">
+        <h2 className="text-center mt-7 font-bold">Week 1</h2>
+        <ClickableButton text="Mon" />
+        <ClickableButton text="Tue" />
+        <ClickableButton text="Wed" />
+        <ClickableButton text="Thu" />
+        <ClickableButton text="Fri" />
+        <ClickableButton text="Sat" />
+        <ClickableButton text="Sun" />
       </div>
       <div className="flex justify-between">
-        <ColoredClickableBtn text="25" />
-        <ColoredClickableBtn text="26" />
-        <ColoredClickableBtn text="27" />
-        <ColoredClickableBtn text="28" />
-        <ColoredClickableBtn text="29" />
-        <ColoredClickableBtn text="30" />
-        <ClickableButton text="1" />
+        <h2 className="text-center mt-7 font-bold">Week 2</h2>
+        <ClickableButton text="Mon" />
+        <ClickableButton text="Tue" />
+        <ClickableButton text="Wed" />
+        <ClickableButton text="Thu" />
+        <ClickableButton text="Fri" />
+        <ClickableButton text="Sat" />
+        <ClickableButton text="Sun" />
       </div>
       <div className="flex justify-between">
-        <ClickableButton text="2" />
-        <ClickableButton text="3" />
-        <ClickableButton text="4" />
-        <ClickableButton text="5" />
-        <ClickableButton text="6" />
-        <ClickableButton text="7" />
-        <ClickableButton text="8" />
+        <h2 className="text-center mt-7 font-bold">Week 3</h2>
+        <ClickableButton text="Mon" />
+        <ClickableButton text="Tue" />
+        <ClickableButton text="Wed" />
+        <ClickableButton text="Thu" />
+        <ClickableButton text="Fri" />
+        <ClickableButton text="Sat" />
+        <ClickableButton text="Sun" />
       </div>
       <div className="flex justify-between">
-        <ClickableButton text="9" />
-        <ClickableButton text="10" />
-        <ClickableButton text="11" />
-        <ClickableButton text="12" />
-        <ClickableButton text="13" />
-        <ClickableButton text="14" />
-        <ClickableButton text="15" />
+        <h2 className="text-center mt-7 font-bold">Week 4</h2>
+        <ClickableButton text="Mon" />
+        <ClickableButton text="Tue" />
+        <ClickableButton text="Wed" />
+        <ClickableButton text="Thu" />
+        <ClickableButton text="Fri" />
+        <ClickableButton text="Sat" />
+        <ClickableButton text="Sun" />
       </div>
       <div className="flex justify-between">
-        <ClickableButton text="16" />
-        <ClickableButton text="17" />
-        <ClickableButton text="18" />
-        <ClickableButton text="19" />
-        <ClickableButton text="20" />
-        <ClickableButton text="21" />
-        <ClickableButton text="22" />
+        <h2 className="text-center mt-7 font-bold">Week 5</h2>
+        <ClickableButton text="Mon" />
+        <ClickableButton text="Tue" />
+        <ClickableButton text="Wed" />
+        <ClickableButton text="Thu" />
+        <ClickableButton text="Fri" />
+        <ClickableButton text="Sat" />
+        <ClickableButton text="Sun" />
       </div>
       <div className="flex justify-between">
-        <ClickableButton text="23" />
-        <ClickableButton text="24" />
-        <ClickableButton text="25" />
-        <ClickableButton text="26" />
-        <ClickableButton text="27" />
-        <ClickableButton text="28" />
-        <ClickableButton text="29" />
+        <h2 className="text-center mt-7 font-bold">Week 6</h2>
+        <ClickableButton text="Mon" />
+        <ClickableButton text="Tue" />
+        <ClickableButton text="Wed" />
+        <ClickableButton text="Thu" />
+        <ClickableButton text="Fri" />
+        <ClickableButton text="Sat" />
+        <ClickableButton text="Sun" />
       </div>
       <div className="flex justify-between">
-        <ClickableButton text="30" />
-        <ClickableButton text="31" />
-        <ColoredClickableBtn text="1" />
-        <ColoredClickableBtn text="2" />
-        <ColoredClickableBtn text="3" />
-        <ColoredClickableBtn text="4" />
-        <ColoredClickableBtn text="5" />
+        <h2 className="text-center mt-7 font-bold">Week 7</h2>
+        <ClickableButton text="Mon" />
+        <ClickableButton text="Tue" />
+        <ClickableButton text="Wed" />
+        <ClickableButton text="Thu" />
+        <ClickableButton text="Fri" />
+        <ClickableButton text="Sat" />
+        <ClickableButton text="Sun" />
       </div>
     </div>
   );
