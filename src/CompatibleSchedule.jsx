5e09f7bd811fee747eae20bfd38920d83006e862
Markus Phantha <markus.a.phantha@gmail.com>
2023-10-23 15:47:12 +0300

    styled the buttons

--- src/CompatibleSchedule.jsx
+++ src/CompatibleSchedule.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const CompatibleSchedule = () => {
   return (
-    <div>
-    <h1>Compatible Schedule</h1>
-    <div className='m-2'>
-      <button className='bg-gray-200 '>Just for fun</button>
-      <button>More serious</button>
-    </div>
-      
-
+    <div className=" flex-col p-60 ">
+      <h1 className="font-bold text-3xl text-orange-400">
+        Compatible Schedule
+      </h1>
+      <div className="m-10 p-2">
+        <button className="bg-gray-200 p-2 rounded-md cursor-pointer">
+          Just for fun
+        </button>
+        <button className="bg-gray-200 p-2 rounded-md cursor-pointer ml-3">
+          More serious
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default CompatibleSchedule
+export default CompatibleSchedule;
