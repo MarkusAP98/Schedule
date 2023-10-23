b7ea870dcc4282b08b7a0bc2aef59699c1202f20
Markus Phantha <markus.a.phantha@gmail.com>
2023-10-23 15:19:33 +0300

    starting others abailibilty

--- src/OthersAvailability.jsx
+++ src/OthersAvailability.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const OthersAvailability = () => {
   return (
-    <div>OthersAvailability</div>
-  )
+    <div>
+    <div className='m-2'>
+      <button className='bg-gray-200 '>Just for fun</button>
+      <button>More serious</button>
+    </div>
+      
+
+    </div>
+  );
 }
 
 export default OthersAvailability
