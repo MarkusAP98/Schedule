3b080cffda1833770581a8e1140a78c78874489e
Markus Phantha <markus.a.phantha@gmail.com>
2023-10-26 17:41:28 +0300

    friends

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import React from "react";
 export default function App() {
 
   return (
-    <div>
-      <h1 className="font-bold p-10">Hello World</h1>
+    <div className="justify-center">
+      <h1 className="font-bold p-10 text-center text-orange-400 text-5xl">Friends</h1>
     </div>
   );
   }
