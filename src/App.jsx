6a993d9bb736c521956e279a0a2b40e56a68688b
Markus Phantha <markus.a.phantha@gmail.com>
2023-10-26 16:06:25 +0300

    restart the challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,11 @@
-import React, { useState } from "react";
-import MyAvailability from "./components/MyAvailability";
-import CompatibleSchedule from "./CompatibleSchedule";
-import Paul from "./components/OthersAvailability.jsx/Paul.jsx"
-;
+import React from "react";
+
 
 export default function App() {
-  const [isDisplayed, setIsDisplayed] = useState("myAvailibility"); // Default to MyAvailability
-
-  const handleMyAvailabilityClick = () => {
-    setIsDisplayed("myAvailibility");
-  };
-
-  const handleOthersAvailabilityClick = () => {
-    setIsDisplayed("others");
-  };
-  const handlePaulAvailabilityClick = () => {
-    setIsDisplayed("Paul");
-  }
 
   return (
     <div>
-      <button
-        className={`ml-5 cursor-pointer hover:underline ${
-          isDisplayed === "myAvailibility" ? "active" : ""
-        }`}
-        onClick={handleMyAvailabilityClick}
-      >
-        My Availability
-      </button>
-      <button
-        className={`ml-5 cursor-pointer hover:underline ${
-          isDisplayed === "others" ? "active" : ""
-        }`}
-        onClick={handleOthersAvailabilityClick}
-      >
-        Compatible Availability
-      </button>
-      <button
-        className={`ml-5 cursor-pointer hover:underline ${
-          isDisplayed === "others" ? "active" : ""
-        }`}
-        onClick={handlePaulAvailabilityClick}
-      >
-        Paul's Availability
-      </button>
-
-      {isDisplayed === "myAvailibility" && (
-        <MyAvailability className="myAvailibility" />
-      )}
-      {isDisplayed === "others" && <CompatibleSchedule className="others" />}
-      {isDisplayed === "Paul" && <Paul className="Paul" />}
+      <h1 className="font-bold p-10">Hello World</h1>
     </div>
   );
-}
+  }
